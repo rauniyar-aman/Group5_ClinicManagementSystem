e6c588bc72257ef20617395bc1062a649ba98e47
rauniyar-aman <amangupta00121212@gmail.com>
2025-12-02 07:49:54 +0545

    Folder Push

---
+++ src/group5_clinicmanagementsystem/Group5_ClinicManagementSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package group5_clinicmanagementsystem;
+
+/**
+ *
+ * @author amang
+ */
+public class Group5_ClinicManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
