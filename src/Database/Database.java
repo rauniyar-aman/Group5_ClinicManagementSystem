baf5533ed25f242b3e23c705e371abcb77bcb012
Probeen001 <209383606+Probeen001@users.noreply.github.com>
2025-12-04 18:42:29 +0545

    Add Database interface and MySqlConnection class for database operations

---
+++ src/Database/Database.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Database;
+import java.sql.*;
+
+
+/**
+ *
+ * @author oakin
+ */
+public interface Database {
+    Connection openconnection();
+    void closeConnection(Connection conn);
+    ResultSet runQuery(Connection conn, String Query);
+    int executeUpdate(Connection conn, String Query);
+    
+}
